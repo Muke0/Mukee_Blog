5165e1b5540cdff8ebb501bfe999630e9f5f9bd8
610759768@qq.com <610759768@qq.com>
2023-04-10 18:31:09 +0800

    logo

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,8 +1,10 @@
 module.exports = {
-  themeConfig: {
-    logo: '/assets/img/logo.png',
-	nav:[
-		
-	]
-  }
+    themeConfig: {
+        base: 1,
+        logo: '/logo/神里宵宫.jpg',
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: '友链', link: '/blogroll' }
+        ]
+    }
 }
