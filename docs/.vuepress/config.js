c97913410e3c38007c104ba39d94955c8a5807aa
610759768@qq.com <610759768@qq.com>
2023-04-10 19:25:00 +0800

    icon

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,10 +1,12 @@
 module.exports = {
     themeConfig: {
+        head: ['link', { rel: 'icon', href: '/logo/icon.jpg' }],
         base: 1,
         logo: '/logo/神里宵宫.jpg',
         nav: [
             { text: 'Home', link: '/' },
-            { text: '友链', link: '/blogroll' }
-        ]
+            { text: '友链', link: '/blogroll/' }
+        ],
+        lastUpdated: 'Last Updated'
     }
 }
--- docs/README.md
+++ docs/README.md
@@ -5,5 +5,4 @@ tagline: Hero 副标题
 features:
 - title: 简洁至上
   details: 以 Markdown 为中心的项目结构，以最少的配置帮助你专注于写作。
-footer: MIT Licensed | Copyright © 2018-present Evan You
 ---
