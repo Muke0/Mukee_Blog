31c0c1381be51d47a1f1a1ac4f9c05b1caba816a
610759768@qq.com <610759768@qq.com>
2023-04-10 19:42:49 +0800

    icon

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,8 +1,10 @@
 module.exports = {
     themeConfig: {
-        head: ['link', { rel: 'icon', href: '/logo/icon.jpg' }],
-        base: 1,
-        logo: '/logo/神里宵宫.jpg',
+        head: [
+            'link', { rel: 'icon', href: '/logo/icon.jpg' }
+        ],
+        //base: 1,
+        logo: '/logo/icon.jpg',
         nav: [
             { text: 'Home', link: '/' },
             { text: '友链', link: '/blogroll/' }
