bb18b0f68ff25cfedf292c515ec5f8dd3708ee88
610759768@qq.com <610759768@qq.com>
2023-05-08 23:12:17 +0800

    导航栏

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,7 +8,21 @@ module.exports = {
         logo: '/logo/唐海音.jpg',
         nav: [
             { text: 'Home', link: '/' },
-            { text: '友链', link: '/blogroll/' }
+            {
+                text: '网络安全',
+                items: [{ text: 'pikachu靶场笔记', link: '/0secure/pikachu/' },
+                    { text: '74cms', link: '/0secure/74cms/' },
+                ]
+            },
+            { text: '友链', link: '/blogroll/' },
+            {
+                text: '靶场',
+                items: [{ text: 'pikachu', link: 'http://www.mukee.ltd:90' },
+                    { text: '74cms', link: 'http://www.mukee.ltd:91' },
+                    { text: 'xdcms', link: 'http://www.mukee.ltd:92' },
+                    { text: '订单系统', link: 'http://www.mukee.ltd:93' }
+                ]
+            }
         ],
         lastUpdated: 'Last Updated'
     }
--- docs/README.md
+++ docs/README.md
@@ -1,9 +1,13 @@
 ---
 home: true
 heroText: Mukee_Blog
-tagline: Hero 副标题
+tagline: 一名计科学生的学习笔记
 features:
-- title: 简洁至上
-  details: 以 Markdown 为中心的项目结构，以最少的配置帮助你专注于写作。
+- title: 网络安全
+  details: 暴力破解，xss，sql注入
+- title: 开发相关
+  details: nodejs
+- title: 运维相关
+  details: nginx,docker
 ---
 [浙ICP备2022032140号](https://beian.miit.gov.cn/)
