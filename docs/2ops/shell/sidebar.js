1db77dfb3c44ebb396173784a68cc2bcd259e8ff
610759768@qq.com <610759768@qq.com>
2023-05-29 20:02:00 +0800

    sidebar

--- docs/2ops/shell/sidebar.js
+++ docs/2ops/shell/sidebar.js
@@ -1,4 +1,9 @@
 module.exports = [{
-    title: 'shell命令',
-    path: '/2ops/shell/command',
-}, ]
+        title: 'shell命令',
+        path: '/2ops/shell/command',
+    },
+    {
+        title: '蓝桥云笔记',
+        path: '/2ops/shell/mark',
+    },
+]
